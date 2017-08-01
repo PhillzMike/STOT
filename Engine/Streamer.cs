791521afc572390c8718c82c37d05acb5eb74db8
Zaida3do <Zaida3dO@bitbucket.org>
2017-08-01 16:10:49 +0100

    Initial Commit

---
+++ Engine/Streamer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Engine
+{
+    /// <summary>
+    /// Class to Help Convert a File of the following file types
+    /// <para>pdf, doc, docx, ppt, pptx, xls, xlsx, txt, html and xml</para>
+    /// <para>to a File Stream</para>
+    /// </summary>
+    //Author 3dO
+    public class Streamer
+    {
+
+    }
+}

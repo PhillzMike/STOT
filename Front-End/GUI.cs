5a7b485c9a56832e7832dc4613fa18aebaeb97e0
Zaida3do <Zaida3dO@bitbucket.org>
2017-08-01 17:14:44 +0100

    Initial Gui Commit

---
+++ Front-End/GUI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Animations;
+using MaterialSkin.Controls;
+
+namespace Front_End {
+    public partial class GUI: MaterialForm {
+        public GUI() {
+            InitializeComponent();
+        }
+    }
+}

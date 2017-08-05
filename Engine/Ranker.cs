65a5d48d3ec94f2013b0608d012b8efd3614b9fe
Phillz <phillzmike@gmail.com>
2017-08-05 14:36:09 +0100

    secondGuy

--- Engine/Ranker.cs
+++ Engine/Ranker.cs
@@ -18,7 +18,17 @@ namespace Engine
         /// <param name="Results">The results.</param>
         /// <returns> A list of documents in descending order of relevance</returns>
         public static List<Document> SearchQuery(List<String> query, Dictionary<Document,int[]> Results) {
+
             return null;
-        } 
+        }
+        private static double TfWeight(int count) {
+            if (count == 0)
+                return 0;
+            else
+                return (1 + Math.Log(count));
+        }
+        private static double IDFWeight(int noOfDocuments) {
+            return Math.Log(InvertedIndexer.Table.)
+        }
     }
 }

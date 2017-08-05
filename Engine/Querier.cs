5644b17ef4c80e4bf60761709be5497e26b02232
Teniola <Teniola@TENIOLAPC>
2017-08-05 09:58:36 +0100

    ooo

--- Engine/Querier.cs
+++ Engine/Querier.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
+using System.Linq;
 
 namespace Engine
 {
@@ -13,10 +13,10 @@ namespace Engine
      
 
         public static List<Document> Search(String query) {
-            //ignore stop words
-            //Separate query into list of words
-            //InvertedIndexer.Table[query].Values
-            //Add all the documents found to a list 
+            //ignore stop words \
+            //Separate query into list of words\
+            //InvertedIndexer.Table[query].Values\
+            //Add all the documents found to a list \
             //return Ranker.SearchQuery(,);
             //type: bring out document of possible type
             //type must be followed by :, no matter the number of whitespaces.
@@ -24,12 +24,12 @@ namespace Engine
            List<String> splitwords = new List<String>();
            var stopwords = File.ReadAllLines("stopwords.txt");
            var stpwordlist = new List<String>(stopwords);
-            foreach (string k in words)
+           foreach (string k in words)
             {
-                if(!stpwordlist.Contains(k))
+                if(!stpwordlist.Contains(k) || words.Contains("type:"))
                    splitwords.Add(k);
-            }
-
+            } 
+            DocsFound(splitwords);
             return null;
         }
         public static List<String> AutoComplete(String query)
@@ -38,21 +38,20 @@ namespace Engine
             return null;
         }
 
-        private static List<Document> DocsFound(List<String> querywords)
+        private static List<KeyValuePair<Document, List<int>>> DocsFound(List<String> querywords)
         {
-            //List<Document> found = new List<Document>();
-            //foreach (string t in querywords)
-            //{
-            //   //ound = InvertedIndexer.Table[t].Values.
- 
-            //}
-            return found;
+             List<KeyValuePair<Document, List<int>>> found = new List<KeyValuePair<Document, List<int>>>();
+            foreach (string t in querywords)
+            {
+                found = InvertedIndexer.Table[t].ToList();
+            
+            }
+             return found;
         }
         //Types : pdf
         //Tokenize query
         //Search Query
         //Autocomplete Query
 
-        //teni is a goat g
     }
 }

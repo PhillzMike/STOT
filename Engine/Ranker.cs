230704514de4ccb6b9511b6060bb74826e42b1f6
Phillz <phillzmike@gmail.com>
2017-08-05 15:30:20 +0100

    stabilizedRanker

--- Engine/Ranker.cs
+++ Engine/Ranker.cs
@@ -17,18 +17,30 @@ namespace Engine
         /// <param name="query">The query.</param>
         /// <param name="Results">The results.</param>
         /// <returns> A list of documents in descending order of relevance</returns>
-        public static List<Document> SearchQuery(List<String> query, Dictionary<Document,int[]> Results) {
+        public static List<Document> SearchQuery(List<String> query, Dictionary<Document,double[]> Results) {
+            
+            foreach (Document item in Results.Keys) {
+                double[] x = Results[item];
+                for(int i = 0; i < x.Length; i++) {
+                    x[i] = TfWeight(x[i]) * IDFWeight(Results.Keys.Count);
+                }
 
+            }
+            
             return null;
         }
-        private static double TfWeight(int count) {
+        private static List<int> Gun(List<string> a) {
+            SortedSet<string> m = new SortedSet<string>(a);
+            return null;
+        }
+        private static double TfWeight(double count) {
             if (count == 0)
                 return 0;
             else
                 return (1 + Math.Log(count));
         }
         private static double IDFWeight(int noOfDocuments) {
-            return Math.Log(InvertedIndexer.Table.)
+            return Math.Log(InvertedIndexer.DocumentCount/noOfDocuments);
         }
     }
 }

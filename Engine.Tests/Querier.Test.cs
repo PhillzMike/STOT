adffe2854b1c2bd23ef20d5d7b40562755bb49a0
Teniola <Teniola@TENIOLAPC>
2017-08-08 23:56:27 +0100

    Possibletype method test and adding and removing of file in updater

--- Engine.Tests/Querier.Test.cs
+++ Engine.Tests/Querier.Test.cs
@@ -14,7 +14,6 @@ namespace Engine.Tests
             Char[] space = { ' ' };
             string[] Actual = s.Split(space,StringSplitOptions.RemoveEmptyEntries);
             string[] expected = { "type",":" };
-            //Assert.AreEqual<string[]>(expected,Actual);
             for(int i = 0;i < expected.Length;i++)
                 Assert.AreEqual<string>(expected[i],Actual[i]);
         }
@@ -26,8 +25,18 @@ namespace Engine.Tests
             string[] sarray = s.Split(space, StringSplitOptions.RemoveEmptyEntries);
             string actual = Querier.TypeChecker(sarray);
             string expected = ("pdf");
-            Assert.AreEqual<string>(expected, actual);
+           Assert.AreEqual<string>(expected, actual);
         }
 
+        [TestMethod]
+        public void PossibleTypeTest()
+        {
+            string s = "p";
+            Format[] sarray = { Format.pdf, Format.ppt, Format.pptx};
+            List<Format> actual = Querier.PossibleType(s);
+            List<Format> expected = new List<Format>(sarray);
+            for (int i = 0; i < actual.Count; i++)
+                Assert.AreEqual<Format>(expected[i], actual[i]);
+        }
     }
 }

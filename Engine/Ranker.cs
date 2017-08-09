7de99b0db3d9ab1055818f40904a061728e87995
Phillz <phillzmike@gmail.com>
2017-08-09 06:33:51 +0100

    Added temp scoring based on relative positions of words

--- Engine/Ranker.cs
+++ Engine/Ranker.cs
@@ -39,7 +39,8 @@ namespace Engine
             int counter = 0;
             List<Document> sortedDocument = new List<Document>();
             foreach (Document item in Results.Keys) {
-                cosOfAngle[counter++] = GetCOSAngle(queryVector, Results[item]);
+                cosOfAngle[counter++] = (GetCOSAngle(queryVector, Results[item])) * 0.7;
+                cosOfAngle[counter] += GetScoreBasedOnPos(query, item) * 0.3;
                 sortedDocument.Add(item);
             }
             //Sorting the documents based on the cosine of the angles
@@ -70,13 +71,92 @@ namespace Engine
             sD = Math.Sqrt(sD);
             return sum / (fD * sD);
         }
-        private static double GetScoreBasedOnPos(List<string> query) {
-            List<Dictionary<Document,List<int>>> positions = new List<Dictionary<Document, List<int>>>();
+        private static double GetScoreBasedOnPos(List<string> query,Document doc) {
+            double score = Math.Tan((45 / query.Count)*ScoreBasedOnConWords(query,doc));
+            return score;
+        }
+        private static double ScoreBasedOnConWords(List<string> query,Document doc) {
+            double finalScore = 0;
+            Dictionary<string, List<int>> positions = new Dictionary<string,List<int>>();
             foreach (var item in query) {
-                positions.Add(Inverter.Table[item]);
+                //TODO: Check this guy
+                if (Inverter.Table[item].ContainsKey(doc))
+                    positions.Add(item, new List<int>(Inverter.Table[item][doc]));
+                else
+                    positions.Add(item, new List<int>());
             }
-              
-            return 0;
+            var wordsDistance = CalculateBestDiff(query, positions);
+            int prevWord = -1;
+            for (int i = 0; i < wordsDistance.Count; i++) {
+                if(wordsDistance[i] != 0) {
+                    //TODO: Check the formula u used here,again
+                    finalScore += (1 - (1 / wordsDistance[i]))/(i-prevWord);
+                    prevWord = i;
+                }
+             
+            }
+            //for(int i = 0; i < query.Count-1; i++) {
+            //    int firstScore = wordsDistance[i];
+            //    if (firstScore == 0)
+            //        continue;
+            //    for (int j = i + 1; j < query.Count; j++) {
+            //        int secondScore = wordsDistance[j];
+            //        if (secondScore == 0)
+            //            continue;
+            //        finalScore = 
+
+            //    }
+                    
+            //}
+            return finalScore;
+        }
+        private static List<int> CalculateBestDiff(List<string> query, Dictionary<string,List<int>> dic) {
+            //TODO: This function should locate the region of the best score used in the document
+            //TODO: I hope this works, cause It would take a bit for me to understand what I've written
+            int score;
+            
+            List<int> output = new List<int>();
+         for(int k = 1; k < query.Count; k++) {
+                //while the first guy is not present
+                while (dic[query[k - 1]].Count == 0) {
+                    score = 0;
+                    continue;
+                }
+                //Like Merge algoruthm in merge sort
+                List<int> first = new List<int>(dic[query[k-1]]);
+                List<int> second = new List<int>(dic[query[k]]);
+                score = int.MaxValue;
+                int i = 0;
+                int j = 0;
+                first.Add(int.MaxValue);
+                second.Add(int.MaxValue);
+                //FIXME: I really need to check the guy
+                while(second.Count == 0) {
+                    score = 0;
+                    output.Add(score);
+                    if (k-1<query.Count)
+                        k++;
+                    else
+                        goto outer;
+                    second = new List<int>(dic[query[k]]);
+                }
+                //Check if the list contains an element first, it would not contain any element if the document does not contain
+                //the word in the first place
+                    //So as to ignore the maxValue guyz
+                    for (int count = 0; count < first.Count + second.Count - 2; count++) {
+                        if (first[i] < second[j]) {
+                            score = (score < first[i] - second[j]) ? score : first[i] - second[j];
+                            i++;
+                        }
+                        else if (first[i] > second[j]) {
+                            score = (score < (second[j] - first[i])) ? score : second[j] - first[i];
+                            j++;
+                        }
+
+                    }
+              outer: output.Add(score);
+            }
+            return output;
         }
         public static double[] GetVector(List<string> a) {
             List<double> voice = new List<double>();

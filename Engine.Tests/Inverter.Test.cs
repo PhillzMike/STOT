bceb88cbc2e11e8b8aa70c4c9289c9a8ffc8acbe
Zaida3do <opsijay@gmail.com>
2017-08-10 05:38:49 +0100

    Moved Test and Backend to Net Framework

--- Engine.Tests/Inverter.Test.cs
+++ Engine.Tests/Inverter.Test.cs
@@ -1,6 +1,8 @@
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Engine;
+using System.IO;
+
 namespace Engine.Tests
 {
     [TestClass]
@@ -9,6 +11,13 @@ namespace Engine.Tests
         [TestMethod]
         public void Inverter()
         {
+            foreach(String a in Directory.EnumerateFiles("../../Mock","*.",SearchOption.AllDirectories)) {
+                try {
+                    DateTime i = new DateTime();
+                    Document doc = new Document("File 1",a,Format.docx,i);
+                    Streamer.AddFileFrom(doc);
+                } catch{}
+            }
             //TODOint i = Engine.Inverter.boy;
         }
     }
---
+++ Engine.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Engine.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Engine.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c01e9fd7-bab9-47a2-b4b0-122571f9c73b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

213eca2494b9c28c39fcddb10914201a61811c8c
Teniola <Teniola@TENIOLAPC>
2017-09-29 10:49:22 +0100

    typechecker removed, updater test

--- Engine/Querier.cs
+++ Engine/Querier.cs
@@ -7,7 +7,7 @@ using System.Text.RegularExpressions;
 
 namespace Engine
 {
-    
+
     /// <summary>
     /// Author Teni
     /// Don't forget to set Querir.invt before you begin
@@ -20,12 +20,13 @@ namespace Engine
         static bool needsRanking;
         public static Inverter Invt { set { invt = value; } }
         static Inverter invt;
-        public static List<Document> Search(String query) {
+        public static List<Document> Search(String query)
+        {
             sw.Start();
             Semanter.Splitwords(query, ":").ToList();
             double t0 = sw.ElapsedMilliseconds;
             //Separate words, remove punctiations,make lowercase
-            List<String> words = Semanter.Splitwords(query,":").ToList();
+            List<String> words = Semanter.Splitwords(query, ":").ToList();
             double t = sw.ElapsedMilliseconds;
             //Obtains possible types searched by this code
             //methods take to long to load y?HashSet<String> typesPossible=TypeChecker(words,invt,t);
@@ -67,19 +68,20 @@ namespace Engine
             }
             double t1 = sw.ElapsedMilliseconds;
             //Fill with all types if no types possible found
-            if (typesPossible.Count == 0) 
+            if (typesPossible.Count == 0)
                 typesPossible.UnionWith(invt.Formats[""]);
             double t2 = sw.ElapsedMilliseconds;
             //Stem words and remove stopwords
             // slower words = words.Except(invt.Stopwords).ToList();
             List<String> splitwords = new List<String>();
             string stem;
-            foreach ( string word in words) {
+            foreach (string word in words)
+            {
                 stem = invt.Samantha.StemWord(word);
                 if (!(invt.Stopwords.Contains(stem)))
                     splitwords.Add(stem);
             }
-            double t3= sw.ElapsedMilliseconds;
+            double t3 = sw.ElapsedMilliseconds;
             if (splitwords.Count == 0)
                 return new List<Document>();
             //search for documents
@@ -89,17 +91,18 @@ namespace Engine
             //throw new ArgumentNullException("File doesn't Exist");
             //TODO "words within quote"
             double t4 = sw.ElapsedMilliseconds;
-            return Ranker.SearchQuery(splitwords, Results, invt.DocumentCount); 
+            return Ranker.SearchQuery(splitwords, Results, invt.DocumentCount);
         }
         public static List<String> AutoComplete(String querywords)
         {
             //TODO review this later
             String[] splitwords = querywords.Split(Semanter.punctuations, StringSplitOptions.RemoveEmptyEntries);
-          //  return Semanter.Suggestions(splitwords);
+            //  return Semanter.Suggestions(splitwords);
             return null;
         }
 
-        private static Dictionary<Document, Dictionary<string, List<int>>> DocsFound(List<String> querywords, HashSet<string> typesPossible) {
+        private static Dictionary<Document, Dictionary<string, List<int>>> DocsFound(List<String> querywords, HashSet<string> typesPossible)
+        {
             needsRanking = false;
             t7 = sw.ElapsedMilliseconds;
             var found = new Dictionary<Document, Dictionary<string, List<int>>>();
@@ -109,13 +112,12 @@ namespace Engine
                 available.UnionWith(invt.AllDocumentsContainingWord(word));
             foreach (Document x in available)
             {
-                if (typesPossible.Contains(x.Type)&& x.Exists)
+                if (typesPossible.Contains(x.Type) && x.Exists)
                 {
                     Dictionary<string, List<int>> wordDict = new Dictionary<string, List<int>>();
                     foreach (string word in querywords)
                     {
 
-                       
                         if (invt.WordIsInDoc(word, x))
                         {
                             if (!wordDict.ContainsKey(word))
@@ -130,7 +132,7 @@ namespace Engine
                                 wordDict.Add(word, new List<int>());
                             }
                         }
-                           
+
                     }
                     found.Add(x, wordDict);
                 }
@@ -142,45 +144,6 @@ namespace Engine
             t10 = sw.ElapsedMilliseconds;
             return found;
         }
-        public static HashSet<string> TypeChecker(List<string> s,double x)
-        {
-            double start = sw.ElapsedMilliseconds;
-            HashSet<string> type = new HashSet<string>();
-            double to;
-        for (int i = 0; i < s.Count; i++) {
-                double ta = sw.ElapsedMilliseconds;
-                if (s[i].Equals("type"))
-                {
-                    if ((i + 2) < s.Count && s[i + 1].Equals(":"))
-                    {
-                        type.UnionWith(invt.Formats[s[i + 2]]);
-                        s.RemoveRange(i,3);
-                        i--;
-                       // s[i] = s[i + 1] = s[i + 2] = "";
-                    }
-                    else if ((i + 1) < s.Count && s[i + 1].StartsWith(":"))
-                    {
-                        type.UnionWith(invt.Formats[s[i + 1].Substring(1)]);
-                        s.RemoveRange(i, 2);
-                        i--;
-                    }
-                }
-                else if ((i + 1) < s.Count && s[i].Equals("type:"))
-                {
-                    type.UnionWith(invt.Formats[s[i + 1]]);
-                    s.RemoveRange(i, 2);
-                    i--;
-                    //s[i] = s[i + 1] = "";
-                }
-                else if (s[i].StartsWith("type:")) {
-                    type.UnionWith(invt.Formats[s[i].Substring(5)]);
-                    s.RemoveAt(i);
-                    i--;
-                }
-                to = sw.ElapsedMilliseconds;
-            }
-            return type;
-    }
 
-    }
+    }           
 }

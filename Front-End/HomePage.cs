bbfbdc1065d00e0e53268072eec9441280ad701e
Zaida3do <opsijay@gmail.com>
2017-10-03 15:18:06 +0100

    Front End cleanUp (HomePage)

--- Front-End/HomePage.cs
+++ Front-End/HomePage.cs
@@ -10,75 +10,52 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 //using Engine;
 
-namespace Front_End
-{
-    public partial class HomePage : Form
-    {
-        BindingList<string> tree = new BindingList<string>();
-        public string SearchTxt { set { TxtSearch.Text = value; TxtSearch.SelectionStart = TxtSearch.Text.Length; } }
-        public SearchPage Searchpage { get; set; }
-        public HomePage()
-        {
+namespace Front_End {
+    public partial class HomePage : Form {
+        public HomePage() {
             InitializeComponent();
         }
-        private void HomePage_Load(object sender, EventArgs e)
-        {
-            listBox1.DataSource = tree;
-            tree.ListChanged += new ListChangedEventHandler(ItemSizeChanged);
+        BindingList<string> SuggestionsList = new BindingList<string>();
+        public string SearchTxt { set { TxtSearch.Text = value; TxtSearch.SelectionStart = TxtSearch.Text.Length; } }
+        public SearchPage Searchpage { get; set; }
+
+        private void HomePage_Load(object sender, EventArgs e) {
+            listBox1.DataSource = SuggestionsList;
+            SuggestionsList.ListChanged += new ListChangedEventHandler(ItemSizeChanged);
             Location = new System.Drawing.Point(0, 0);
         }
-        private void ItemSizeChanged(object sender,ListChangedEventArgs e)
-        {
-            if (listBox1.Items.Count == 0)
-            {
-                listBox1.Visible = false;
-                listBox1.Height = 0;
+        private void ItemSizeChanged(object sender, EventArgs e) {
 
-            }
-            else {
-                listBox1.Height = (28+(24*(listBox1.Items.Count-1)));
-                listBox1.Visible = true;
-            }
         }
-        private void MaterialRaisedButton1_Click(object sender, EventArgs e)
-        {
+        private void MaterialRaisedButton1_Click(object sender, EventArgs e) {
             if (!OpenSearchPage()) { }
-            //tree.Add("cdrghrghirtjjbrgigijg"+i++);
         }
         private bool OpenSearchPage() {
-            if ((!TxtSearch.Text.Equals(""))&&Querier.Search(TxtSearch.Text).Count > 0)
-            {
+            if ((!TxtSearch.Text.Equals("")) && Querier.Search(TxtSearch.Text).Count > 0) {
                 Searchpage.SearchTxt = TxtSearch.Text;
                 Searchpage.BringToFront();
-                
+
                 return true;
-            }
-            else
-            {
+            } else {
                 return false;
             }
-               
-        }
-        private void ListBox1_ItemSelected(object sender, EventArgs e)
-        {
 
         }
-        private void ListBox1_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            if (e.KeyChar == 13)
-            {
+        private void ListBox1_ItemSelected(object sender, EventArgs e) {
+            TxtSearch.Text = listBox1.SelectedItem.ToString();
+        }
+        private void ListBox1_KeyPress(object sender, KeyPressEventArgs e) {
+            if (e.KeyChar == 13) {
                 ListBox1_ItemSelected(sender, e);
             }
         }
-        private void TxtSearch_TextChanged(object sender, EventArgs e)
-        {            
-            tree.Clear();
+        private void TxtSearch_TextChanged(object sender, EventArgs e) {
+            SuggestionsList.Clear();
+            foreach (string s in Querier.AutoComplete(TxtSearch.Text, 4))
+                SuggestionsList.Add(s);
             OpenSearchPage();
-            /*  foreach (string it in Engine.Querier.AutoComplete(TxtSearch.Text))
-                  tree.Add(it);*/
-            //Below Lines will open the search page the moment Querier.Search starts returning search Results
-           
+
         }
-        
+
     }
 }
